4e7eb91b78da1b6fe8450da501b0f80ab1848cc7
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-10-03 12:12:55 +0200

    Removed the extra copyright paragraph

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -4,7 +4,6 @@ import debounce from 'lodash/debounce';
 $(document).ready(function() {
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
     $('body').append('<button>Click here to get started</button>');
     $('body').append('<p id=\'count\'></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
