c01c09da0aa6b2ee81ebb16c67ca5878ed9a4f24
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-10-03 16:22:41 +0200

    Adding CSS & Images

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+require('../css/main.css');
+var $ = require('jquery');
+var _ = require('lodash');
+import logo from '../assets/holberton-logo.jpg';
+
+$(document).ready(function() {
+    $('body').append('<div id="logo"></div>')
+    $('#logo').css('background-image', `url(${logo})`);
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    let count = 0;
+
+    function updateCounter() {
+        count += 1;
+        $('#count').text(`${count} clicks on the button`);
+    }
+    $('button').on('click', _.debounce(updateCounter, 500));
+});
+
