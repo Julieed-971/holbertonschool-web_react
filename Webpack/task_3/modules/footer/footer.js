dbd35c77fc26a6d7cfd9760fc817c0831d9a3cd8
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-21 10:32:48 +0100

    - Developed utility tests

--- Webpack/task_3/modules/footer/footer.js
+++
@@ -1,8 +0,0 @@
-require('./footer.css');
-var $ = require('jquery');
-
-$(document).ready(function() {
-    var footer = document.createElement('footer')
-    $('body').after(footer)
-    $('footer').append('<p>Copyright - Holberton School</p>');
-})
