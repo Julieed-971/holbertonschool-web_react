8064637f5150dced816e766cfdcdc7f983f8eb17
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-21 13:41:22 +0100

    Test for the notifications list

---
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import Notifications from './Notifications'
+
+test('Renders the notifications title', () => {
+    render(<Notifications />)
+    expect(screen.getByText(/^here is the list of notifications$/i)).toBeInTheDocument()
+})
+
+test('Renders a button in the notifications', () => {
+    render(<Notifications />)
+    expect(screen.getByRole('button')).toBeInTheDocument(   )
+})
+
+test('Renders exactly 3 li elements', () => {
+    render(<Notifications />)
+    expect(screen.getAllByRole('listitem').length).toBe(3)
+})
+
+test('Clicking the close button logs Close button has been clicked to the console', () => {
+    render(<Notifications />)
+    console.log = jest.fn()
+    fireEvent.click(screen.getByRole('button'))
+    expect(console.log).toHaveBeenCalledWith("Close button has been clicked")
+})
