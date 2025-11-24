7c5755988ab8347dc978d444fa4bdd5fb3d904b1
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-24 16:47:41 +0100

    Added missing tests for input focus when corresponding labels are clicked

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -1,11 +1,29 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Login from './Login'
 
 test('renders 2 labels, 2 inputs and 1 button element', () => {
     render(<Login />)
-    expect(screen.getByLabelText('Email:')).toBeInTheDocument()
-    expect(screen.getByLabelText('Password:')).toBeInTheDocument()
+    expect(screen.getByText('Email:')).toBeInTheDocument()
+    expect(screen.getByText('Password:')).toBeInTheDocument()
     expect(screen.getByRole('textbox', {name: /^email:$/i})).toBeInTheDocument()
     expect(screen.getByLabelText(/^password:$/i)).toBeInTheDocument()
     expect(screen.getByRole('button', {name: /^ok$/i})).toBeInTheDocument()
+})
+
+test('inputs elements get focused whenever the related label is clicked', async () => {
+    const user = userEvent.setup()
+    render(<Login />)
+    const emailLabel = screen.getByText('Email:').closest('label')
+    const emailInput = screen.getByRole('textbox', {name: /^email:$/i})
+
+    await user.click(emailLabel)
+
+    expect(emailInput).toHaveFocus()
+
+    const passwordLabel = screen.getByText('Password:').closest('label')
+    const passwordInput = screen.getByLabelText(/^password:$/i)
+
+    await user.click(passwordLabel)
+    expect(passwordInput).toHaveFocus()
 })
