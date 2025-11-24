b96c825caa982e63a3674d7db266cbb8fdffcc90
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-24 16:51:14 +0100

    Regroup in one test?

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -2,18 +2,14 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from './Login'
 
-test('renders 2 labels, 2 inputs and 1 button element', () => {
+test('renders 2 labels, 2 inputs and 1 button element and that inputs elements get focused whenever the related label is clicked', async () => {
     render(<Login />)
     expect(screen.getByText('Email:')).toBeInTheDocument()
     expect(screen.getByText('Password:')).toBeInTheDocument()
     expect(screen.getByRole('textbox', {name: /^email:$/i})).toBeInTheDocument()
     expect(screen.getByLabelText(/^password:$/i)).toBeInTheDocument()
     expect(screen.getByRole('button', {name: /^ok$/i})).toBeInTheDocument()
-})
-
-test('inputs elements get focused whenever the related label is clicked', async () => {
     const user = userEvent.setup()
-    render(<Login />)
     const emailLabel = screen.getByText('Email:').closest('label')
     const emailInput = screen.getByRole('textbox', {name: /^email:$/i})
 
