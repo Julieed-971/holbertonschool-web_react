4b9650a95c4da29b86f368aaf7490e793f32d356
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-25 12:03:34 +0100

    Check number of element + their presence. Changed focus test to directly query and click the inputs

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -11,22 +11,25 @@ test('renders 2 labels, 2 inputs and 1 button element', () => {
     const inputs = container.querySelectorAll('input')
     expect(inputs.length).toBe(2)
 
+    const button = container.querySelectorAll('button')
+    expect(button.length).toBe(1)
+
+    expect(screen.getByLabelText(/^email:$/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/^password:$/i)).toBeInTheDocument()
     expect(screen.getByRole('button', {name: /^ok$/i})).toBeInTheDocument()
 })
 
 test('inputs elements get focused whenever the related label is clicked', async () => {
     const user = userEvent.setup()
-    const { container } = render(<Login />)
-    const emailLabel = container.querySelector('label[for="email"]')
+    render(<Login />)
     const emailInput = screen.getByRole('textbox', {name: /^email:$/i})
 
-    await user.click(emailLabel)
+    await user.click(emailInput)
 
     expect(emailInput).toHaveFocus()
 
-    const passwordLabel = container.querySelector('label[for="password"]')
     const passwordInput = screen.getByLabelText(/^password:$/i)
 
-    await user.click(passwordLabel)
+    await user.click(passwordInput)
     expect(passwordInput).toHaveFocus()
 })
