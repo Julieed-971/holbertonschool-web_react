e58ea8d82119eb92a135816f82f35d76c2dc006d
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-25 10:35:51 +0100

    Modified login tests to test the actual count of label, inputs and button elements

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -2,14 +2,21 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from './Login'
 
-test('renders 2 labels, 2 inputs and 1 button element and that inputs elements get focused whenever the related label is clicked', async () => {
-    render(<Login />)
-    expect(screen.getByText('Email:')).toBeInTheDocument()
-    expect(screen.getByText('Password:')).toBeInTheDocument()
-    expect(screen.getByRole('textbox', {name: /^email:$/i})).toBeInTheDocument()
-    expect(screen.getByLabelText(/^password:$/i)).toBeInTheDocument()
-    expect(screen.getByRole('button', {name: /^ok$/i})).toBeInTheDocument()
+test('renders 2 labels, 2 inputs and 1 button element', () => {
+    const { container } = render(<Login />)
+    
+    const labels = container.querySelectorAll('label')
+    expect(labels).toHaveLength(2)
+
+    const inputs = container.querySelectorAll('input')
+    expect(inputs).toHaveLength(2)
+
+    expect(screen.getAllByRole('button')).toHaveLength(1)
+})
+
+test('inputs elements get focused whenever the related label is clicked', async () => {
     const user = userEvent.setup()
+    render(<Login />)
     const emailLabel = screen.getByText('Email:').closest('label')
     const emailInput = screen.getByRole('textbox', {name: /^email:$/i})
 
