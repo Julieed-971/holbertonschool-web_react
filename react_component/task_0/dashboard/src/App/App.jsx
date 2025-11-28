9e9fa36c4731846ab2649638f814e7fa74797c9a
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-28 10:17:51 +0100

    Switch to class components

--- react_component/task_0/dashboard/src/App/App.jsx
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import React from 'react'
 import Notifications from '../Notifications/Notifications'
 import Header from '../Header/Header'
 import Login from '../Login/Login'
@@ -6,41 +7,51 @@ import Footer from '../Footer/Footer'
 import CourseList from '../CourseList/CourseList'
 import { getLatestNotification } from '../utils/utils'
 
-function App({ isLoggedIn = false }) {
-  const notificationsList = [
-    {id: 1, type: "default", value: "New course available"},
-    {id: 2, type: "urgent", value: "New resume available"},
-    {id: 3, type: "urgent", html: { __html: getLatestNotification() }},
-  ]
-  const coursesList = [
-    {id: 1, name: "ES6", credit: 60},
-    {id: 2, name: "Webpack", credit: 20},
-    {id: 3, name: "React", credit: 40}
-  ]
+class App extends React.Component  {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      notificationsList: [
+        {id: 1, type: "default", value: "New course available"},
+        {id: 2, type: "urgent", value: "New resume available"},
+        {id: 3, type: "urgent", html: { __html: getLatestNotification() }},
+      ],
+      coursesList: [
+        {id: 1, name: "ES6", credit: 60},
+        {id: 2, name: "Webpack", credit: 20},
+        {id: 3, name: "React", credit: 40}
+      ]
+    }
+  }
 
-  return (
-    <>
-      <div className="root-notifications">
-        <Notifications notifications={notificationsList}/>
-      </div>
-      <div className="root-header">
-        <Header />
-      </div>
-      {isLoggedIn ? (
-        <div className="root-courses">
-          <CourseList courses={coursesList}/>
-        </div>    
-        ) : (
-          <div className="root-login">
-            <Login />
-          </div>
-        )
-      }
-      <div className="root-footer">
-        <Footer />
-      </div>
-    </>
-  )
+  render() {
+    const { isLoggedIn = false } = this.props
+    const { notificationsList, coursesList } = this.state
+
+    return (
+      <>
+        <div className="root-notifications">
+          <Notifications notifications={notificationsList}/>
+        </div>
+        <div className="root-header">
+          <Header />
+        </div>
+        {isLoggedIn ? (
+          <div className="root-courses">
+            <CourseList courses={coursesList}/>
+          </div>    
+          ) : (
+            <div className="root-login">
+              <Login />
+            </div>
+          )
+        }
+        <div className="root-footer">
+          <Footer />
+        </div>
+      </>
+    )
+  }
 }
-
 export default App
