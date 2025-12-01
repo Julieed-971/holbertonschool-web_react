0a739a7193f3f06aad8569438fca37af7cbd4de2
Julie Dedieu <julie.dedieu1@gmail.com>
2025-12-01 10:29:36 +0100

    Handling Events: - Transformed Notifications and NotificationItem from functionnal to class component - Added a mark as read log when a notification is clicked

---
+++ react_component/task_2/dashboard/src/App/App.jsx
@@ -0,0 +1,76 @@
+import './App.css'
+import React from 'react'
+import Notifications from '../Notifications/Notifications'
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+import CourseList from '../CourseList/CourseList'
+import { getLatestNotification } from '../utils/utils'
+
+class App extends React.Component  {
+  static defaultProps = {
+    isLoggedIn: false,
+    logOut: () => {}
+  }
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      notificationsList: [
+        {id: 1, type: "default", value: "New course available"},
+        {id: 2, type: "urgent", value: "New resume available"},
+        {id: 3, type: "urgent", html: { __html: getLatestNotification() }},
+      ],
+      coursesList: [
+        {id: 1, name: "ES6", credit: 60},
+        {id: 2, name: "Webpack", credit: 20},
+        {id: 3, name: "React", credit: 40}
+      ]
+    }
+  }
+
+  handleLogout = (event) => {
+      if (event.ctrlKey && event.key === "h") {
+        alert('Logging you out')
+        this.props.logOut()
+      }
+    }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleLogout)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleLogout)
+  }
+
+  render() {
+    const { isLoggedIn = false } = this.props
+    const { notificationsList, coursesList } = this.state
+
+    return (
+      <>
+        <div className="root-notifications">
+          <Notifications notifications={notificationsList} displayDrawer={false}/>
+        </div>
+        <div className="root-header">
+          <Header />
+        </div>
+        {isLoggedIn ? (
+          <div className="root-courses">
+            <CourseList courses={coursesList}/>
+          </div>    
+          ) : (
+            <div className="root-login">
+              <Login />
+            </div>
+          )
+        }
+        <div className="root-footer">
+          <Footer />
+        </div>
+      </>
+    )
+  }
+}
+export default App
---
+++ react_component/task_2/dashboard/src/App/App.spec.js
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from './App.jsx'
+
+test('renders login and copyright paragraph with the correct content', async () => {
+    render(<App />)
+    expect(screen.getByText(/^login to access the full dashboard$/i)).toBeInTheDocument()
+    expect(screen.getByText(/^copyright/i)).toBeInTheDocument()   
+})
+
+test('renders Email and Password label element', async () => {
+    render(<App />)
+    expect(screen.getByText(/^email:$/i)).toBeInTheDocument()
+    expect(screen.getByText(/^password:$/i)).toBeInTheDocument()
+})
+
+test('renders the Login component when isLoggedIn is false', () => {
+    render(<App isLoggedIn={false} />)
+    expect(screen.getByText(/^login to access the full dashboard$/i)).toBeInTheDocument()
+})
+
+test('renders the CourseList component when isLoggedIn is true', () => {
+    render(<App isLoggedIn={true} />)
+    expect(screen.getByRole('table')).toBeInTheDocument()
+})
+
+test('verify that logOut is called once when ctrl+h are pressed', () => {
+    const mockLogOut = jest.fn()
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation()
+
+    render(<App logOut={mockLogOut}/>)
+
+    fireEvent.keyDown(document, {key: 'h', ctrlKey: true})
+    expect(mockLogOut).toHaveBeenCalledTimes(1)
+    alertSpy.mockRestore()
+})
+
+test('checks that alert function is called with "Logging you out" message', () => {
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation()
+    render(<App />)
+    fireEvent.keyDown(document, {
+        key: 'h',
+        ctrlKey: true
+    })
+    expect(alertSpy).toHaveBeenCalledWith('Logging you out')
+    alertSpy.mockRestore()
+})
