5c436de5e4baa164ca164e4eec8c0c0b5b9c464b
Julie Dedieu <julie.dedieu1@gmail.com>
2025-12-02 15:35:27 +0100

    Task 2 initial commit

---
+++ react_styling/task_2/dashboard/src/CourseList/CourseListRow.jsx
@@ -0,0 +1,39 @@
+function CourseListRow({
+    isHeader= false, 
+    textFirstCell = "",
+    textSecondCell = null
+}) {
+    if (isHeader) {
+        if (textSecondCell === null) {
+            return (
+                <tr className="bg-[var(--color-table-header)]/66">
+                    <th className="border border-gray-400" colSpan={2}>
+                        {textFirstCell}
+                    </th>
+                </tr>
+            )
+        }
+        return (
+            <tr className="bg-[var(--color-table-header))]/66">
+                <th className="border border-gray-400">
+                    {textFirstCell}
+                </th>
+                <th className="border border-gray-400">
+                    {textSecondCell}
+                </th>
+            </tr>
+        )
+    }
+    return (
+        <tr className="bg-[var(--color-table-rows))]/45">
+            <td className="border border-gray-400 pl-2">
+                {textFirstCell}
+            </td>
+            <td className="border border-gray-400 pl-2">
+                {textSecondCell}
+            </td>
+        </tr>
+    )
+}
+
+export default CourseListRow
