04a3a43d9ee61a3d83c97045237641ff80307327
Julie Dedieu <julie.dedieu1@gmail.com>
2025-12-03 10:12:10 +0100

    Update the Notifications Panel

--- react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -18,16 +18,15 @@ class Notifications extends React.Component {
         const { displayDrawer = false, notifications = [] } = this.props
         return (
             <>
-                <div className="notification-title  absolute right-3 top-1">Your notifications</div>
+                <div className="notification-title">Your notifications</div>
                 {displayDrawer && (
-                    <div className="notification-items  relative border-[3px] border-dotted border-[color:var(--main-color)] p-1.5 w-1/4 float-right mt-7">
+                    <div className="notification-items">
                         {notifications.length === 0 ? (
                             <p>No new notification for now</p>
                         ) : (
                             <>
-                            <div className="relative">
-                                <p className='m-0'>Here is the list of notifications</p>
-                                <ul className='list-[square] pl-5'>
+                                <p>Here is the list of notifications</p>
+                                <ul>
                                     {notifications.map((notification) => (
                                         <NotificationItem
                                             key={notification.id}
@@ -39,14 +38,21 @@ class Notifications extends React.Component {
                                         />
                                     ))}
                                 </ul>
-                                </div>
                             </>
                         )}
 
-                        <button className="absolute cursor-pointer right-0 top-0 bg-transparent"
+                        <button style={{
+                            position: 'absolute',
+                            top: '2px',
+                            right: '2px',
+                            background: 'transparent',
+                            border: 'none',
+                            color: '#343434',
+                            cursor: 'pointer',
+                        }}
                             onClick={() => console.log("Close button has been clicked")}
                             aria-label='Close'>
-                            <img src={closeButton} alt="close-button" className="w-3 h-3"/>
+                            <img src={closeButton} alt="close-button" />
                         </button>
                     </div>
                 )}
