86923b8b4c051a1e92ffc3369c9d6c80191a07af
Julie Dedieu <julie.dedieu1@gmail.com>
2026-01-05 23:17:27 +0100

    Refactor authSlice and tests for improved state management

--- react_redux-part1/task_1/dashboard/src/features/auth/authSlice.js
+++ react_redux-part1/task_1/dashboard/src/features/auth/authSlice.js
@@ -1,25 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    user: {
-        email: '',
-        password: '',
-        isLoggedIn: false,
-    },
-}
+  user: {
+    email: '',
+    password: '',
+  },
+  isLoggedIn: false,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        login(state, action) {
-            state.user = { ...action.payload, isLoggedIn: true }
-        },
-        logout(state) {
-            state.user = initialState.user
-        }
-    }
-})
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user.email = action.payload.email;
+      state.user.password = action.payload.password;
+      state.isLoggedIn = true;
+    },
+    logout: (state) => {
+      state.user.email = '';
+      state.user.password = '';
+      state.isLoggedIn = false;
+    },
+  },
+});
 
-export const { login, logout } = authSlice.actions
-export default authSlice.reducer
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
