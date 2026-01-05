4a1b6d6a12eeb671ba3eca72b3f35a7822fe8a1d
Julie Dedieu <julie.dedieu1@gmail.com>
2026-01-05 23:37:07 +0100

    Enhance test setup by mocking console.log to suppress WithLogging output

--- react_redux-part1/task_1/dashboard/setupTests.js
+++ react_redux-part1/task_1/dashboard/setupTests.js
@@ -1,6 +1,12 @@
 import '@testing-library/jest-dom';
 import { StyleSheetTestUtils } from 'aphrodite';
 
+// Mock console.log to suppress WithLogging output
+global.console = {
+  ...console,
+  log: jest.fn(),
+};
+
 beforeEach(() => {
   StyleSheetTestUtils.suppressStyleInjection();
 });
