28c3c5caab7e65564f23844f2b3e4a9bf85032c0
Julie Dedieu <julie.dedieu1@gmail.com>
2026-02-13 14:26:30 +0100

    Added tests for CourseListRow component

--- react_redux-part2/task_2/dashboard/src/pages/CourseList/CourseListRow/CourseListRow.spec.js
+++ react_redux-part2/task_2/dashboard/src/pages/CourseList/CourseListRow/CourseListRow.spec.js
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen, within, fireEvent } from '@testing-library/react';
 import CourseListRow from './CourseListRow';
 
 test('it should display 1 "th" element with colspan=2 when isHeader is true and textSecondCell is null', () => {
@@ -43,4 +43,61 @@ test('it should render 2 "td" elements inside a "tr" element when isHeader is fa
 
   expect(trElement).toBeInTheDocument();
   expect(tdElements).toHaveLength(2);
+});
+
+test('it should render a checkbox when isHeader is false', () => {
+  render(
+    <table>
+      <tbody>
+        <CourseListRow isHeader={false} textFirstCell="Data1" textSecondCell="Data2" />
+      </tbody>
+    </table>
+  )
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).toBeInTheDocument();
+});
+
+test('checkbox should be checked when isSelected is true', () => {
+  render(
+    <table>
+      <tbody>
+        <CourseListRow isHeader={false} isSelected={true} textFirstCell="Data1" textSecondCell="Data2" />
+      </tbody>
+    </table>
+  )
+  const checkbox = screen.getByRole('checkbox');
+
+  expect(checkbox).toBeChecked();
+});
+
+test('checkbox should be unchecked when isSelected is false', () => {
+  render(
+    <table>
+      <tbody>
+        <CourseListRow isHeader={false} isSelected={false} textFirstCell="Data1" textSecondCell="Data2" />
+      </tbody>
+    </table>
+  )
+  const checkbox = screen.getByRole('checkbox');
+
+  expect(checkbox).not.toBeChecked();
+
+});
+
+test('onChangeRow is called with correct parameters when checkbox is clicked', () => {
+  const mockOnChangeRow = jest.fn();
+
+  render(<CourseListRow
+    id={1}
+    isSelected={false}
+    onChangeRow={mockOnChangeRow}
+    textFirstCell={'data1'}
+    textSecondCell={'data2'}
+  />);
+
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+  
+  expect(mockOnChangeRow).toHaveBeenCalledWith(1, true);
 });
